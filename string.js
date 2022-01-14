0862f1773fc84a06c58ab9a149e791ebdd1dfffe
Matias Esteban <estebanmatias92@gmail.com>
2022-01-14 19:32:34 -0300

    missingChar()
    Add missingChar to find the first consecutive missing value from alphabet in a ordered string

--- string.js
+++ string.js
@@ -1,7 +1,6 @@
 /*
   Converts ANY string to "spinal-case"
 */
-
 function spinalCase(str) {
   str = str.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
   
@@ -19,4 +18,20 @@ function sensitiveReplace(str, before, after) {
       replace[0] = capitalChar.test(before) ? replace[0].toUpperCase() : replace[0].toLowerCase();      
       return replace.join("");
   });
+}
+
+
+/*
+  Find the missing char in an ordered alphabet string, returnns the first consecutive missing value from alphabet
+*/
+function missingChar(str) {
+  
+  for (let i = 0; i < str.length; i++) {    
+    let current = str.charCodeAt(i);
+    let actualChar = str.charCodeAt(0) + i;
+    if (current !== actualChar)
+      return String.fromCharCode(actualChar);
+  }
+  
+  return undefined;
 }
