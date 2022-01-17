f1362aca0ace3dacb2bad35b2470dd0ba8a9fd4d
Matias Esteban <estebanmatias92@gmail.com>
2022-01-17 05:21:27 -0300

    adding getPrimes()

--- math.js
+++ math.js
@@ -9,3 +9,25 @@ Object.assign(Math, {
     'microLcm': (a, b) => a * b / Math.microGcd(a, b),
     'lcm': (...arr) => arr.reduce((prev, value, i) => prev = (i === 0 ? value : Math.microLcm(value, prev)), 1)
 });
+
+
+/*
+  Sieve of Eratosthenes
+  
+  The function marks every composite index (every multiple of "i") as true, this way only the primes are left undefined or falsy, to add then to the array.
+*/
+function getPrimes(num) {
+    let sieve = [];
+    let primes = [];
+
+    for (let i = 2; i <= num; i++) {
+        if (!sieve[i]) {
+            primes.push(i);
+
+            for (let j = i * 2; j <= num; j += i)
+                sieve[j] = true;          
+        }
+    }
+
+    return primes;
+}
