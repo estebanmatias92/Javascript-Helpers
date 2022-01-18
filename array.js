f16e33d086d35eb72ffa01a1c5ab8999a8b9098e
Matias Esteban <estebanmatias92@gmail.com>
2022-01-18 05:55:19 -0300

    Add range()

--- array.js
+++ array.js
@@ -19,3 +19,15 @@ function _mergeUnique(arr) {
   }
   return uniques;  
 }
+
+
+/*
+   Simple range(start, end) function to generate an array
+*/
+function range(start, end) {
+    var ans = [];
+    for (let i = start; i <= end; i++) {
+        ans.push(i);
+    }
+    return ans;
+}
